d6105dc0ee8a0c2f785addc920aa0725b1ac51bb
uzmasyed00 <uzmasyed00@gmail.com>
2015-05-24 23:14:12 +1000

    Added more functionality

--- sportingItems.py
+++ sportingItems.py
@@ -20,9 +20,46 @@ def CategoriesNew():
         session.commit()
         category_id = session.query(Categories).filter_by(name = category.name).one().c_id
         print "category_id is", category_id
-        return render_template('CategoryCreated.html')
+        return redirect(url_for('ShowCategories'))
     else:
         return render_template('newCategory.html')
+    
+@app.route('/category/<int:category_id>/item/new',methods = ['GET','POST'])
+def newItem(category_id):
+    if request.method =='POST':
+        item = Items(name = request.form['name'],description = request.form['description'],category_id = category_id)
+        session.add(item)
+        session.commit()
+        return render_template('ItemCreated.html')
+    else:
+        return render_template('newItem.html')
+    
+@app.route('/category/<int:category_id>/item',methods = ['GET'])    
+def ShowItems(category_id):
+    print "I am inside ShowItems"
+    category = session.query(Categories).filter_by(c_id = category_id).one()
+    items = session.query(Items).filter_by(category_id = category_id).all()
+    #if request.method == 'POST':        
+    for item in items:
+        print item.name
+        #return render_template('newItem.html')
+    #else:
+    return render_template('items.html', items = items, category = category)
+
+@app.route('/category/<int:category_id>/item/<int:item_id>/edit',methods = ['GET','POST'])
+def editItem(category_id, item_id):
+    category_to_edit = session.query(Categories).get(category_id)
+    item_to_edit = session.query(Items).get(item_id)
+    if request.method == 'POST':
+        return redirect(url_for('ShowItems'),category_id)
+        print "I am in if statement"
+    else:
+        return render_template('editItem.html', category_id = category_id, item_id = item_id)
+        #return render_template('editItem.html', category_to_edit, item_to_edit)
+
+@app.route('/category/<int:category_id>/item/<int:item_id>/delete',methods = ['GET','POST'])
+def deleteItem(category_id, item_id):
+    print "I am going to delete the item"
     
 @app.route('/categories',methods = ['GET']) 
 def ShowCategories():
@@ -33,8 +70,9 @@ def ShowCategories():
 
 @app.route('/category/<int:category_id>/edit',methods = ['GET','POST'])
 def editCategory(category_id):
+    #isnt category_id defined as c_id, then how is it accepting category_id?
     category_to_edit = session.query(Categories).get(category_id)
-    print "category_name to be edited is",category_to_edit
+    #print "category_name to be edited is",category_to_edit
     if request.method =='POST':
         category_name = request.form['name']
         category_to_edit.name = category_name
@@ -42,7 +80,7 @@ def editCategory(category_id):
         return redirect(url_for('ShowCategories'))
     else:
         #return render_template('editCategory.html', category =category_to_edit)
-        print "I am going to render editCategory.html"
+        #print "I am going to render editCategory.html"
         return render_template('editCategory.html',category = category_to_edit)
 
 @app.route('/category/<int:category_id>/delete',methods = ['GET','POST'])
